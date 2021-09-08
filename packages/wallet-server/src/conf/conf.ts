6158e63dd797b6aab55a7f409ae29536ae99ce43
Valentin <valentin@Valentins-iMac.local>
2021-09-08 15:17:37 -0700

    udpate MAC DMG

--- packages/wallet-server/src/conf/conf.ts
+++ packages/wallet-server/src/conf/conf.ts
@@ -6,11 +6,11 @@ export const addTESTNETNodeServer = '5.9.150.112:19335';
 export const defaultDirObj = {
     WINDOWS: `${homedir()}/AppData/Roaming/Litecoin`,
     LINUX: `${homedir()}/.litecoin/`,
-    MAX: `${homedir()}/Library/Application Support/Litecoin/`,
+    MAC: `${homedir()}/Library/Application Support/Litecoin/`,
 };
 
 export const coreFilePathObj = {
     WINDOWS: join(__dirname, 'litecoind.exe'),
     LINUX: join(__dirname, 'litecoind'),
-    MAX: join(__dirname, 'litecoind-mac'),
+    MAC: join(__dirname, 'litecoind-mac'),
 };
