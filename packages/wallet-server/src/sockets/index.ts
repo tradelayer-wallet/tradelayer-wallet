67a5945050c2741f941a8e210ae8ed3941131c65
Valentin <dev.peshev@gmail.com>
2022-01-26 15:26:31 +0200

    update wallet/server  version

--- packages/wallet-server/src/sockets/index.ts
+++ packages/wallet-server/src/sockets/index.ts
@@ -7,8 +7,8 @@ export let walletSocketSevice: WalletSocketSevice;
 export let serverSocketService: ServerSocketService;
 
 export const myVersions = {
-    nodeVersion: '0.0.1',
-    walletVersion: '0.0.1',
+    nodeVersion: '0.0.2',
+    walletVersion: '0.0.2',
 };
 
 export const initWalletConnection = (app: FastifyInstance, socketScript: SocketScript) => {
