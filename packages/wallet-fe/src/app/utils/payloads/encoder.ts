a809505bb174c286055db0843f2c06025f38f899
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2024-06-20 15:12:38 -0400

    Update encoder.ts
    More functions to fill out the channel trades.

--- packages/wallet-fe/src/app/utils/payloads/encoder.ts
+++ packages/wallet-fe/src/app/utils/payloads/encoder.ts
@@ -40,4 +40,84 @@ const encodeTradeTokensChannel = (params: TradeTokensChannelParams): string => {
     return payload.join(',');
 };
 
-export const ENCODER = { encodeSend,encodeTradeTokensChannel };
+type EncodeWithdrawalParams = {
+    withdrawAll: boolean;
+    propertyId: number;
+    amount: number;
+    column: number;
+    channelAddress: string;
+};
+
+const encodeWithdrawal = (params: EncodeWithdrawalParams): string => {
+    const withdrawAll = params.withdrawAll;
+    const propertyIds = params.propertyId.toString(36);
+    const amounts = params.amount.toString(36);
+    const column = params.column; // 0 is A, 1 is B
+    return [withdrawAll, propertyIds, amounts, column, params.channelAddress].join(',');
+};
+
+type EncodeTradeContractChannelParams = {
+    contractId: number;
+    price: number;
+    amount: number;
+    columnAIsSeller: boolean;
+    expiryBlock: number;
+    insurance: boolean;
+};
+
+const encodeTradeContractChannel = (params: EncodeTradeContractChannelParams): string => {
+    const payload = [
+        params.contractId.toString(36),
+        params.price.toString(36),
+        params.amount.toString(36),
+        params.columnAIsSeller ? '1' : '0',
+        params.expiryBlock.toString(36),
+        params.insurance ? '1' : '0',
+    ];
+    return payload.join(',');
+};
+
+type EncodeTradeTokenForUTXOParams = {
+    propertyId: number;
+    amount: number;
+    columnA: string;
+    satsExpected: number;
+    tokenOutput: string;
+    payToAddress: string;
+};
+
+const encodeTradeTokenForUTXO = (params: EncodeTradeTokenForUTXOParams): string => {
+    const payload = [
+        params.propertyId.toString(36),
+        params.amount.toString(36),
+        params.columnA,
+        params.satsExpected.toString(36),
+        params.tokenOutput,
+        params.payToAddress,
+    ];
+    return payload.join(',');
+};
+
+type EncodeCommitParams = {
+    propertyId: number;
+    amount: number;
+    channelAddress: string;
+};
+
+const encodeCommit = (params: EncodeCommitParams): string => {
+    const payload = [
+        params.propertyId.toString(36),
+        params.amount.toString(36),
+        params.channelAddress,
+    ];
+    return payload.join(',');
+};
+
+export const ENCODER = { 
+    encodeSend, 
+    encodeTradeTokensChannel, 
+    encodeWithdrawal, 
+    encodeTradeContractChannel, 
+    encodeTradeTokenForUTXO, 
+    encodeCommit 
+};
