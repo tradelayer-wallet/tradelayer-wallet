689d67c6e1aae58e59999dc753f3ab95d3b726e7
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-07-15 16:40:48 -0400

    Revert "logging orders cleanup"
    This reverts commit 0184060d6749e98e8f95909af4e0170bc9f81420.

--- packages/wallet-fe/src/app/@core/services/futures-services/futures-orderbook.service.ts
+++ packages/wallet-fe/src/app/@core/services/futures-services/futures-orderbook.service.ts
@@ -98,12 +98,6 @@ export class FuturesOrderbookService {
 
     subscribeForOrderbook() {
         this.endOrderbookSbuscription();
-
-        this.socket.on(`${obEventPrefix}::connect`, () => {
-        // Clear local orderbook data immediately
-        this.rawOrderbookData = [];
-        });
-
         this.socket.on(`${obEventPrefix}::order:error`, (message: string) => {
             this.toastrService.error(message || `Undefined Error`, 'Orderbook Error');
             this.loadingService.tradesLoading = false;
